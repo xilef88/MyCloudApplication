da30b73ef79a7d99bd365671573bb7fad869b4ee
roumblestein <37830998+roumblestein@users.noreply.github.com>
2020-05-28 00:52:27 +0200

    jg

--- app/src/main/java/com/example/mycloudapplication/LoginActivity.java
+++ app/src/main/java/com/example/mycloudapplication/LoginActivity.java
@@ -27,8 +27,6 @@ public class LoginActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
 
-
-
         provider = Arrays.asList(
                 new AuthUI.IdpConfig.EmailBuilder().build()
         );
